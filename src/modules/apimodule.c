4c3f708e6e96621abf6bf41694a47996c0d8d676
runping.jin <runping.jin@huixiaoer.com>
2019-08-08 19:39:21 +0800

    add stringMoudlueAPi

--- src/modules/apimodule.c
+++ src/modules/apimodule.c
@@ -4,12 +4,28 @@
 #include <ctype.h>
 #include <string.h>
 
+/** 获取时间 */
 int redisModule_Milliseconds(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
     long long millSeconds = RedisModule_Milliseconds();
     printf("RedisModule_Milliseconds $hd", millSeconds);
     RedisModule_ReplyWithLongLong(ctx, millSeconds);
     return REDISMODULE_OK;
 }
+/** 获取截止字符串 */
+int redisModule_StringPtrLen(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+    const char *str = RedisModule_StringPtrLen(*argv, 1);
+    RedisModule_ReplyWithString(ctx, str);
+    return REDISMODULE_OK;
+}
+
+/** 创建module string */
+int redisModule_CreateString(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+    RedisModuleString *str = RedisModule_CreateString(ctx, *argv, sizeof(*argv));
+    RedisModule_ReplyWithLongLong(sizeof(str));
+    RedisModule_Free(str);
+    return REDISMODULE_OK;
+}
+
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (RedisModule_Init(ctx, "module_test", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR){
@@ -23,6 +39,14 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     }
 
     if (RedisModule_CreateCommand(ctx,"api_RedisModule_Milliseconds",redisModule_Milliseconds, "readonly",0,0,0) == REDISMODULE_ERR){
+        return REDISMODULE_ERR;
+    }
+
+    if (RedisModule_CreateCommand(ctx,"api_RedisModule_StringPtrLen",redisModule_Milliseconds, "readonly",0,0,0) == REDISMODULE_ERR){
+        return REDISMODULE_ERR;
+    }
+
+    if (RedisModule_CreateCommand(ctx,"api_RedisModule_CreateString",redisModule_CreateString, "readonly",1,1,1) == REDISMODULE_ERR){
         return REDISMODULE_ERR;
     }
 
